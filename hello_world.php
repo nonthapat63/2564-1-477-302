aa5a9d54f5bfb7adde548c15f07289ba01c82bde
nonthapat63 <71367639+nonthapat63@users.noreply.github.com>
2021-09-06 15:52:59 +0700

    PHP

---
+++ hello_world.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        //test hello world #course id: 477-302
+        /* semester 1/2021*/
+        
+        /* echo "Hello World in PHP! without HTML!";
+
+        */
+        echo "Hello World in PHP! without HTML!"; 
+        $txt = "Hello World";
+        $age = 40;
+        $txt2 = "years old.";
+        $_txt1= "";
+        echo $txt;
+        echo $age;
+        echo $txt2;
+        echo $txt1 . $age . $txt2;
+    ?>
+    <div style="color:red">
+        <h1>
+            <?php echo "Hello World in PHP! without HTML!" ?>
+        </h1>
+    </div> 
+</body>
+</html>
