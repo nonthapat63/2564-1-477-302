d9d24f502d0b3a7535c1a4ebce0dfeb407fb84b5
nonthapat63 <71367639+nonthapat63@users.noreply.github.com>
2021-10-26 12:03:07 +0700

    Delete header.php

--- header.php
+++
@@ -1,6 +0,0 @@
-<div class="row ws-head">
-    <div class="col-xs-12 col-sm-12">
-        <div class="ws-name"><span style="color:#333333;">MY</span><span style="color:#3399CC">WEBBOARD</span></div>
-        <div><span style="color:#828282">เว็บบอร์ด</span></div>
-    </div>
-</div>
